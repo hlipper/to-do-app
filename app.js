59aab5ff4100ffe81077035faa0d02bd3d57c9ba
Holly Lipper <hollylipper@gmail.com>
2019-04-27 08:54:01 -0500

    Checkpoint 5 Revision Request 1 Complete

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//for Checkpoint 5 To-Do-App Part 1
+
 function onReady(){
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
@@ -8,13 +10,31 @@ function onReady(){
     let title = newToDoText.value;
 
     let newLi = document.createElement('li');
+    newLi.classList.add('mdl-list__item', 'newMdl');
+    let toDoContainer = document.createElement('span');
+    toDoContainer.classList.add('mdl-list__item-primary-content', 'newMdl');
+    let toDoLabel = document.createElement('label');
+    toDoLabel.classList.add('mdl-checkbox', 'mdl-js-checkbox', 'mdl-js-ripple-effect', 'newMdl');
+    toDoLabel.htmlFor = 'list-checkbox-1';
     let checkbox = document.createElement('input');
     checkbox.type = "checkbox";
+    checkbox.id = 'list-checkbox-1';
+    checkbox.classList.add('mdl-checkbox__input', 'newMdl');
+
+    let deleteContainer = document.createElement('span');
+    deleteContainer.classList.add('mdl-list__item-secondary-action', 'newMdl');
+
 
     let deleteButton = document.createElement('button');
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener('click', event => {
+    deleteButton.classList.add('mdl-button', 'mdl-js-button', 'mdl-button--icon', 'newMdl');
+    let icon = document.createElement('i');
+    let text = document.createTextNode('delete');
+    icon.classList.add('material-icons');
+    icon.appendChild(text);
+    deleteButton.appendChild(icon);
 
+    deleteButton.addEventListener('click', event => {
+      newLi.parentNode.removeChild(newLi);
     });
 
 
